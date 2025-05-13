b6977dd5bcce5fa32d6c29047b25a458b36a4606
Kyrsten Carlson <kyrstenfeb11@gmail.com>
2025-05-13 22:49:34 +0000

    application override

--- apps/demo/webpack.config.js
+++ apps/demo/webpack.config.js
@@ -9,12 +9,18 @@ module.exports = (env) => {
   webpack.chainWebpack((config) => {
     // shared demo code
     config.resolve.alias.set('@demo/shared', resolve(__dirname, '..', '..', 'tools', 'demo'));
+
+    if (webpack.Utils.platform.getPlatformName() === 'android') {
+      // make sure the path to the application.android.(js|ts)
+      // is relative to the webpack.config.js
+      config.entry('application').add('./application.android')
+    }
   });
 
   // Example if you need to share images across demo apps:
   // webpack.Utils.addCopyRule({
-  //   from: '../../../tools/images', 
-	// 	to: 'images',
+  //   from: '../../../tools/images',
+  // 	to: 'images',
   //   context: webpack.Utils.project.getProjectFilePath('node_modules')
   // });
 
